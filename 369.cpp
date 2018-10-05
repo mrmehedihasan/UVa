bb5b5d6d79943712194d72bc425059277f654f9e
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2018-10-05 16:50:39 +0600

    Update 369.cpp

--- 369.cpp
+++ 369.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
+int nCr(int n, int r){
+    if(n==r || r==0){
+        return 1;
+    }else if(r==1){
+        return n;
+    }else{
+        return nCr(n-1,r-1)+nCr(n-1, r);
+    }
+}
+
+int main(){
+    
+    
+    int a,b;
+    while(scanf("%d %d", &a, &b)){
+        if(a==0 && b==0){
+            break;
+        }
+        printf("%d things taken %d at a time is %d exactly.\n", a,b,nCr(a,b));
+    }
+    return 0;
+}
